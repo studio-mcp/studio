5f122c61e8cd15f0f6f205d4485d5a6e45a4ff88
Martin Emde <me@martinemde.com>
2025-06-11 19:32:26 -0700

    Initial

---
+++ lib/studio/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Studio
+  VERSION = "0.1.0"
+end
---
+++ spec/studio_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Studio do
+  it "has a version number" do
+    expect(Studio::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
